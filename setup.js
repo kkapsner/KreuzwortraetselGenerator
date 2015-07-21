b36f740834b748603df7a514b0a4d2243325cbb1
kkapsner <github@mail.kkapsner.de>
2015-07-21 23:30:40 +0200

    Code cleanup.

--- setup.js
+++ setup.js
@@ -1,260 +1,281 @@
 (function(){
 
-var questionsNode;
-var lastQuestionRow = null;
-var addQuestion = function _addQuestion(question, answer){
-	var row = kkjs.node.create({
-		tag: "tr",
-		parentNode: questionsNode,
-		childNodes: [
-			{tag: "td", childNodes: [{tag: "span", innerHTML: "&#x2195", className: "handle"}]},
-			{tag: "td", childNodes: [{tag: "input", name: "question[]", value: question}]},
-			{tag: "td", childNodes: [{tag: "input", name: "answer[]", value: answer}]}
-		]
+var questions = function(){
+	var questionsNode;
+	var lastQuestionRow = null;
+	kkjs.event.onDOMReady(function(){
+		questionsNode = kkjs.$("questions");
+		kkjs.event.add(kkjs.$("addQuestion"), "click", function(){api.add();});
 	});
-	row.question = kkjs.css.$("input[name^=question]", {node: row})[0];
-	row.answer = kkjs.css.$("input[name^=answer]", {node: row})[0];
-	lastQuestionRow = row;
-	
-	kkjs.event.add(row.answer, "blur", function(ev){
-		if (this.parentNode.parentNode === lastQuestionRow && this.value){
-			addQuestion();
-		}
-	});
-	kkjs.event.add.key(row, "ENTER", "down", function(ev){
-		if (ev.target === row.question){
-			row.answer.focus();
-		}
-		if (ev.target === row.answer){
-			if (this === lastQuestionRow){
-				ev.target.blur();
-				lastQuestionRow.question.focus();
-			}
-			else {
-				this.nextSibling.question.focus();
-			}
-		}
-	});
-	
-	kkjs.node.create({
-		tag: "td",
-		parentNode: row,
-		childNodes: [{
-			tag: "button",
-			innerHTML: "&minus;",
-			onclick: function(){
-				removeQuestion(row);
-			}
-		}]
-	});
-	kkjs.sortable.dragNDrop.unset(questionsNode);
-	kkjs.sortable.dragNDrop.set(questionsNode, {handleSelector: "span.handle"});
-}.setDefaultParameter("", "");
-function removeQuestion(row){
-	kkjs.node.remove(row);
-	lastQuestionRow = questionsNode.rows[questionsNode.rows.length - 1];
-}
-function removeAllQuestions(){
-	while (questionsNode.rows.length){
-		removeQuestion(questionsNode.rows[0]);
-	}
-}
-var getQuestions = function getQuestions(filter){
-	var questionNodes = kkjs.css.$("input[name^=question]", {node: questionsNode});
-	var questions = kkjs.css.$("input[name^=answer]", {node: questionsNode}).map(function(node, i){
-		return {question: questionNodes[i].value, answer: node.value};
-	});
-	if (filter){
-		questions = questions.filter(function(o){
-			return o.answer.length !== 0;
-		});
-	}
-	questions.forEach(function(question, i){
-		question.number = i;
-	});
-	return questions;
-}.setDefaultParameter(true);
-
-kkjs.event.onWindowLoad(function(){
-	
-	
-	var lastGrid;
-	function placeSolution(){
-		if (lastGrid){
-			lastGrid.solution = [];
-			var solution = Grid.normalizeWord(kkjs.$("solution").value);
-			if (solution){
-				var chars = {};
-				
-				for (var i = 0; i < 26; i++){
-					var c = String.fromCharCode("a".charCodeAt(0) + i);
-					chars[c] = lastGrid.chars[c].slice();
+	var api = {
+		add: function _addQuestion(question, answer){
+			var row = kkjs.node.create({
+				tag: "tr",
+				parentNode: questionsNode,
+				childNodes: [
+					{tag: "td", childNodes: [{tag: "span", innerHTML: "&#x2195", className: "handle"}]},
+					{tag: "td", childNodes: [{tag: "input", name: "question[]", value: question}]},
+					{tag: "td", childNodes: [{tag: "input", name: "answer[]", value: answer}]}
+				]
+			});
+			row.question = kkjs.css.$("input[name^=question]", {node: row})[0];
+			row.answer = kkjs.css.$("input[name^=answer]", {node: row})[0];
+			lastQuestionRow = row;
+			
+			kkjs.event.add(row.answer, "blur", function(ev){
+				if (this.parentNode.parentNode === lastQuestionRow && this.value){
+					api.add();
 				}
-				
-				for (var i = 0; i < solution.length; i++){
-					var c = solution[i];
-					if (chars[c].length){
-						var idx = Math.floor(Math.random() * chars[c].length);
-						lastGrid.solution.push(chars[c][idx].clone());
-						chars[c].splice(idx, 1);
+			});
+			kkjs.event.add.key(row, "ENTER", "down", function(ev){
+				if (ev.target === row.question){
+					row.answer.focus();
+				}
+				if (ev.target === row.answer){
+					if (this === lastQuestionRow){
+						ev.target.blur();
+						lastQuestionRow.question.focus();
 					}
 					else {
-						lastGrid.solution = [];
-						alert("L\xF6sungwort ist nicht in Antworten enthalten");
-						break;
+						this.nextSibling.question.focus();
 					}
 				}
-			}
-			lastGrid.update();
-		}
-		else {
-			alert("Es muss erst ein Kreuzwortr\xE4tsel erstellt werden.");
-		}
-	}
-	function generate(){
-		kkjs.node.clear(kkjs.$("schema"));
-		
-		var questions = getQuestions();
-		
-		if (questions.length === 0){
-			alert("Keine W\xF6rter angegeben.");
-			return;
-		}
-		
-		kkjs.random.array.shuffle(questions);
-		
-		lastGrid = Grid.create(questions);
-		
-		questions.sort(function(a, b){
-			a = a.number;
-			b = b.number;
-			return a > b? 1: (b > a? -1: 0);
-		});
-		
-		
-		kkjs.$("placeSolution").disabled =  !lastGrid;
-		if (lastGrid){
-			lastGrid.trim();
-			placeSolution();
+			});
 			
-			var list = kkjs.node.create({
-				tag: "ol",
-				parentNode: kkjs.$("schema")
+			kkjs.node.create({
+				tag: "td",
+				parentNode: row,
+				childNodes: [{
+					tag: "button",
+					innerHTML: "&minus;",
+					onclick: function(){
+						api.remove(row);
+					}
+				}]
 			});
-			questions.forEach(function(q){
-				kkjs.node.create({
-					tag: "li",
-					parentNode: list,
-					childNodes: [q.question]
+			kkjs.sortable.dragNDrop.unset(questionsNode);
+			kkjs.sortable.dragNDrop.set(questionsNode, {handleSelector: "span.handle"});
+		}.setDefaultParameter("", ""),
+		remove: function removeQuestion(row){
+			kkjs.node.remove(row);
+			lastQuestionRow = questionsNode.rows[questionsNode.rows.length - 1];
+		},
+		removeAll: function removeAllQuestions(){
+			while (questionsNode.rows.length){
+				api.remove(questionsNode.rows[0]);
+			}
+		},
+		get: function getQuestions(filter){
+			var questionNodes = kkjs.css.$("input[name^=question]", {node: questionsNode});
+			var questions = kkjs.css.$("input[name^=answer]", {node: questionsNode}).map(function(node, i){
+				return {question: questionNodes[i].value, answer: node.value};
+			});
+			if (filter){
+				questions = questions.filter(function(o){
+					return o.answer.length !== 0;
 				});
+			}
+			questions.forEach(function(question, i){
+				question.number = i;
 			});
-			kkjs.$("schema").appendChild(lastGrid.createNode());
+			return questions;
+		}.setDefaultParameter(true)
+	};
+	return api;
+}();
+
+var solution = function(){
+	var solutionNode;
+	var display;
+	kkjs.event.onDOMReady(function(){
+		solutionNode = kkjs.$("solution");
+		display = kkjs.$("solutionDisplay")
+		kkjs.event.add(solutionNode, "input", api.display);
+		kkjs.event.add(kkjs.$("placeSolution"), "click", api.place);
+		api.display();
+	});
+	var api = {
+		set: function(solution){
+			solutionNode.value = solution;
+			api.display();
+		},
+		get: function(){
+			return solutionNode.value;
+		},
+		display: function(){
+			kkjs.node.clear(display);
+			var solution = Grid.normalizeWord(solutionNode.value, true);
+			var normalisedSolution = Grid.normalizeWord(solutionNode.value);
+			
+			var start = true;
+			for (var idx = 0, nIdx = 0; idx < solution.length; idx += 1){
+				if (solution[idx] === normalisedSolution[nIdx]){
+					kkjs.node.create({
+						tag: "span",
+						parentNode: display,
+						className: "char" + (start? " start": ""),
+						childNodes: [
+							{
+								tag: "span",
+								className: "number",
+								childNodes: (nIdx + 1).toString()
+							},
+							{
+								tag: "span",
+								className: "answer",
+								childNodes: solution[idx]
+							}
+						]
+					});
+					nIdx += 1;
+					start = false;
+				}
+				else {
+					kkjs.node.create({
+						tag: "span",
+						parentNode: display,
+						childNodes: solution[idx]
+					});
+					start = true;
+				}
+			}
+		},
+		place: function(){
+			var lastGrid = grid.get();
+			if (lastGrid){
+				lastGrid.solution = [];
+				var solution = Grid.normalizeWord(solutionNode.value);
+				if (solution){
+					var chars = {};
+					
+					for (var i = 0; i < 26; i++){
+						var c = String.fromCharCode("a".charCodeAt(0) + i);
+						chars[c] = lastGrid.chars[c].slice();
+					}
+					
+					for (var i = 0; i < solution.length; i++){
+						var c = solution[i];
+						if (chars[c].length){
+							var idx = Math.floor(Math.random() * chars[c].length);
+							lastGrid.solution.push(chars[c][idx].clone());
+							chars[c].splice(idx, 1);
+						}
+						else {
+							lastGrid.solution = [];
+							alert("L\xF6sungwort ist nicht in Antworten enthalten");
+							break;
+						}
+					}
+				}
+				lastGrid.update();
+			}
+			else {
+				alert("Es muss erst ein Kreuzwortr\xE4tsel erstellt werden.");
+			}
 		}
-		else {
-			kkjs.$("downloadImage").href = "";
-			alert("Kein zusammneh\xE4ngendes R\xE4tsel m\xF6glich.");
+	};
+	return api;
+}();
+
+var grid = function(){
+	var lastGrid = null;
+	var lastTable = null;
+	var api = {
+		generate: function(){
+			var q = questions.get();
+			
+			if (q.length === 0){
+				alert("Keine W\xF6rter angegeben.");
+				return;
+			}
+			
+			kkjs.random.array.shuffle(q);
+			
+			var grid = Grid.create(q);
+			
+			
+			if (grid){
+				grid.trim();
+				api.set(grid);
+				solution.place();
+			}
+			else {
+				alert("Kein zusammneh\xE4ngendes R\xE4tsel m\xF6glich.");
+			}
+		},
+		display: function(){
+			if (lastGrid){
+				kkjs.node.clear(kkjs.$("schema"));
+				kkjs.$("placeSolution").disabled =  !lastGrid;
+				
+				var list = kkjs.node.create({
+					tag: "ol",
+					parentNode: kkjs.$("schema")
+				});
+				questions.get().forEach(function(q){
+					kkjs.node.create({
+						tag: "li",
+						parentNode: list,
+						childNodes: [q.question]
+					});
+				});
+				lastTable = lastGrid.createNode();
+				kkjs.$("schema").appendChild(lastTable);
+			}
+		},
+		get: function(){
+			return lastGrid;
+		},
+		set: function(grid){
+			lastGrid = grid;
+			lastTable = null;
+			api.display();
+		},
+		getCanvas: function(){
+			return Grid.tableToCanvas(lastTable);
 		}
-	}
+	};
+	return api;
+}();
+
+
+kkjs.event.onWindowLoad(function(){
+	kkjs.event.add(kkjs.$("generate"), "click", grid.generate);
 	
-	kkjs.event.add(kkjs.$("generate"), "click", generate);
 	
+	kkjs.localFile.bounce.URL = "http://bounce.kkapsner.de/echoToDownload.php";
+	kkjs.event.add(kkjs.$("saveImage"), "click", function(ev){
+		if (grid.get()){
+			kkjs.localFile.save("grid.png", {dataURL: grid.getCanvas().toDataURL()});
+		}
+	});
 	kkjs.event.add(kkjs.$("save"), "click", function(){
+		var lastGrid = grid.get();
 		if (lastGrid){
 			var data = {
 				grid: lastGrid,
-				questions: getQuestions(),
-				solution: kkjs.$("solution").value
+				questions: questions.get(),
+				solution: solution.get()
 			};
 			kkjs.localFile.save("crossword.json", JSON.stringify(data), "application/json");
 		}
 	});
+	
 	function loadFilePromiseCallback(){
 		var data = JSON.parse(this.content);
-		removeAllQuestions();
+		questions.removeAll();
 		
-		kkjs.$("solution").value = data.solution;
+		solution.set(data.solution);
 		data.questions.forEach(function(q){
-			addQuestion(q.question, q.answer);
+			questions.add(q.question, q.answer);
 		});
 		
-		lastGrid = Grid.fromObject(data.grid);
-		kkjs.node.clear(kkjs.$("schema"));
-		kkjs.$("placeSolution").disabled =  !lastGrid;
-		
-		kkjs.$("schema").appendChild(lastGrid.createNode());
-		var list = kkjs.node.create({
-			tag: "ol",
-			parentNode: kkjs.$("schema")
-		});
-		data.questions.forEach(function(q){
-			kkjs.node.create({
-				tag: "li",
-				parentNode: list,
-				childNodes: [q.question]
-			});
-		});
+		grid.set(Grid.fromObject(data.grid));
 	}
 	kkjs.event.add(kkjs.$("load"), "click", function(){
 		kkjs.localFile.load(loadFilePromiseCallback);
 	});
 	kkjs.localFile.enableFileDrop(document, loadFilePromiseCallback);
-	
-	kkjs.event.add(kkjs.$("downloadImage"), "mouseover", function(){
-		if (lastGrid){
-			this.href = Grid.tableToCanvas(kkjs.$("schema").firstChild).toDataURL();
-		}
-		else {
-			this.removeAttribute("href");
-		}
-	});
-	kkjs.localFile.bounce.URL = "http://bounce.kkapsner.de/echoToDownload.php";
-	kkjs.event.add(kkjs.$("downloadImage"), "click", function(ev){
-		if ((kkjs.is && kkjs.is.ie) || navigator.userAgent.indexOf("Trident") !== -1){
-			kkjs.localFile.save("grid.png", {dataURL: this.href});
-			ev.preventDefault();
-		}
-	});
-	kkjs.event.add(kkjs.$("placeSolution"), "click", placeSolution);
-	kkjs.event.add(kkjs.$("solution"), "input", function(){
-		var display = kkjs.$("solutionDisplay");
-		kkjs.node.clear(display);
-		var solution = Grid.normalizeWord(this.value, true);
-		var normalisedSolution = Grid.normalizeWord(this.value);
-		
-		var start = true;
-		for (var idx = 0, nIdx = 0; idx < solution.length; idx += 1){
-			if (solution[idx] === normalisedSolution[nIdx]){
-				kkjs.node.create({
-					tag: "span",
-					parentNode: display,
-					className: "char" + (start? " start": ""),
-					childNodes: [
-						{
-							tag: "span",
-							className: "number",
-							childNodes: (nIdx + 1).toString()
-						},
-						{
-							tag: "span",
-							className: "answer",
-							childNodes: solution[idx]
-						}
-					]
-				});
-				nIdx += 1;
-				start = false;
-			}
-			else {
-				kkjs.node.create({
-					tag: "span",
-					parentNode: display,
-					childNodes: solution[idx]
-				});
-				start = true;
-			}
-		}
-	}).fireEvent("input");
-	kkjs.event.add(kkjs.$("addQuestion"), "click", function(){addQuestion();});
 	
 	kkjs.event.add(kkjs.$("displayAnswer"), ["change", "click"], function(){
 		kkjs.css.className[this.checked? "add": "remove"](kkjs.$("schema"), "answered");
@@ -262,25 +283,26 @@ kkjs.event.onWindowLoad(function(){
 	}).fireEvent("click");
 });
 
+// cookie storage
 kkjs.event.onWindowLoad(function(){
 	questionsNode = kkjs.$("questions");
-	kkjs.$("solution").value = kkjs.cookie.getValue("crosswordSolution");
+	solution.set(kkjs.cookie.getValue("crosswordSolution"));
 	var data = kkjs.cookie.getValue("crosswordData");
 	if (data){
 		try {
 			data = JSON.parse(data);
 			data.forEach(function(q){
-				addQuestion(q.question, q.answer);
+				questions.add(q.question, q.answer);
 			});
 			return;
 		}
 		catch(e){}
 	}
-	addQuestion();
+	questions.add();
 });
 kkjs.event.add(window, "unload", function(){
-	kkjs.cookie.setValue("crosswordData", JSON.stringify(getQuestions(false)));
-	kkjs.cookie.setValue("crosswordSolution", kkjs.$("solution").value);
+	kkjs.cookie.setValue("crosswordData", JSON.stringify(questions.get(false)));
+	kkjs.cookie.setValue("crosswordSolution", solution.get());
 });
 
 })();
